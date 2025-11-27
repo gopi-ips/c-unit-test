61dbd4f295bdf4fb6ab6bcec54e7b5726eeb4b34
gopi-ips <gopinaath.c@ips-ai.com>
2025-11-27 20:58:28 +0530

    updated tasks.c

--- tasks.c
+++ tasks.c
@@ -22,7 +22,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
+ * https://github.com/FreeRTOS additions
  *
  */
 
